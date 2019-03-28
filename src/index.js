4c8cf0d4674e53d48a40bff71f1cc531563c02bd
duongha <friendd1996@gmail.com>
2019-03-29 01:07:16 +0700

    add render attributes

--- src/index.js
+++ src/index.js
@@ -1,52 +1,4 @@
-const dom  = Symbol('dom');
-const vdom = Symbol('vdom');
-
-function render(vdom, parent) {
-    const renderDom = parent ? (dom) => parent.appendChild(dom) : (dom) => dom;
-    if (typeof vdom != 'object') {
-        return renderDom(document.createTextNode(vdom))
-    }
-
-    if (typeof vdom.type === 'string') {
-        const dom = renderDom(document.createElement(vdom.type));
-        [].concat(...vdom.children).map(child => render(child, dom));
-        return dom
-    }
-
-    const props = {children: vdom.children, ...vdom.props};
-
-    if (typeof vdom.type === 'function' && vdom.type.prototype instanceof Component) {
-        const component = new vdom.type(props);
-        component[dom]  = render(component.render(), parent);
-        return component[dom]
-    }
-
-    if (typeof vdom.type === 'function') {
-        return render(new vdom.type(props), parent)
-    }
-
-    throw new Error(`Invalid VDOM: ${vdom}.`);
-}
-
-function update(vdom, dom, parent = dom.parentNode) {
-    if (typeof vdom != 'object' && parent) {
-        const newDom = document.createTextNode(vdom)
-        parent.replaceChild(newDom, dom);
-        return newDom
-    }
-}
-
-class Component {
-    constructor(props) {
-
-    }
-
-
-    setState(newState) {
-        this.state = {...newState};
-        this[dom]  = update(this.render(), this[dom])
-    }
-}
+import React, { Component } from './react';
 
 class Bar extends Component {
 
@@ -58,39 +10,33 @@ class Bar extends Component {
 
         setInterval(() => {
             this.setState({count: this.state.count + 1})
-        }, 1);
+        }, 100);
 
     }
 
 
     render() {
-        return (this.state.count);
+        return <Demo>{Date.now()}</Demo>;
     }
 }
 
-const React = {
-    createElement(type, props = {}, ...children) {
-        return {type, props, children};
-    },
-    render,
-};
+class Demo extends Component {
+
+    render() {
+        return <Foo>{this.props.children}</Foo>;
+    }
+}
 
 const Foo = (props) => {
-    return <div>{props.children}</div>
+    return <div style={{
+        color: '#' + ((1 << 24) * Math.random() | 0).toString(16),
+    }}>
+        {props.children}
+    </div>
 };
 
-const Test = () => <div>
-    <div>
-        <div>
-            <div>tada</div>
-        </div>
-        <Foo>foo</Foo>
-        <Bar>bar</Bar>
-    </div>
-</div>;
 
-
-React.render(<Bar> foo</Bar>, document.getElementById('root'));
+React.render(<Bar>{Date.now()}</Bar>, document.getElementById('root'));
 
 
 
--- src/react.js
+++ src/react.js
@@ -1,43 +1,76 @@
-const dom = Symbol('dom');
+const dom    = Symbol('dom');
+const events = Symbol('events');
 
 function render(vdom, parent) {
-    const renderDom = parent ? (dom) => parent.appendChild(dom) : (dom) => dom;
+    const newDom = buildNewDom(vdom);
+    parent && parent.appendChild(newDom);
+
+    return dom
+}
+
+const nativeAttributes = new Set(['checked', 'value', 'className']);
+
+function update(vdom, dom, parent = dom.parentNode) {
+    const newDom = buildNewDom(vdom);
+    parent.replaceChild(newDom, dom);
+    return newDom
+}
+
+function buildNewDom(vdom) {
     if (typeof vdom != 'object') {
-        return renderDom(document.createTextNode(vdom))
+        return document.createTextNode(vdom)
     }
 
     if (typeof vdom.type === 'string') {
-        const dom = renderDom(document.createElement(vdom.type));
-        [].concat(...vdom.children).map(child => render(child, dom));
-        return dom
+        const newDom = document.createElement(vdom.type);
+        [].concat(...vdom.children).map(child => newDom.appendChild(buildNewDom(child)));
+        for (const prop in vdom.props) {
+            if (vdom.props.hasOwnProperty(prop)) {
+                setAttribute(newDom, prop, vdom.props[prop])
+            }
+        }
+
+        return newDom
     }
 
-    const props = {children: vdom.children, ...vdom.props};
+    if (typeof vdom.type !== 'function') {
+        throw new Error(`Invalid VDOM: ${vdom}.`);
+    }
 
-    if (typeof vdom.type === 'function' && vdom.type.prototype instanceof Component) {
+    const props = {children: [].concat(...vdom.children), ...vdom.props};
+
+    if (vdom.type.prototype instanceof Component) {
         const component = new vdom.type(props);
-        component[dom]  = render(component.render(), parent);
+        component[dom]  = buildNewDom(component.render());
         return component[dom]
     }
 
-    if (typeof vdom.type === 'function') {
-        return render(new vdom.type(props), parent)
-    }
-
-    throw new Error(`Invalid VDOM: ${vdom}.`);
+    return buildNewDom(new vdom.type(props))
 }
 
-function update(vdom, dom, parent = dom.parentNode) {
-    if (typeof vdom != 'object' && parent) {
-        const newDom = document.createTextNode(vdom)
-        parent.replaceChild(newDom, dom);
-        return newDom
+function setAttribute(dom, key, val) {
+    if (nativeAttributes.has(key)) {
+        return dom[key] = val;
     }
+
+    if (key === 'style' && typeof val == 'object') {
+        return Object.assign(dom.style, val)
+    }
+
+    if (typeof val == 'function' && key.startsWith('on')) {
+        const eventType = key.slice(2).toLowerCase();
+        dom[events]     = dom[events] || {};
+        dom.removeEventListener(eventType, dom[events][eventType]);
+        dom[events][eventType] = val;
+        return dom.addEventListener(eventType, dom[events][eventType]);
+    }
+
+    dom.setAttribute(key, val)
 }
 
 class Component {
     constructor(props) {
-
+        this.props = props
     }
 
 
@@ -45,14 +78,22 @@ class Component {
         this.state = {...newState};
         this[dom]  = update(this.render(), this[dom])
     }
+
+    render() {
+        return null
+    }
 }
 
+function createElement(type, props = {}, ...children) {
+    return {type, props, children};
+}
 
-export default {
-    createElement(type, props, ...children) {
-        return {type, props, children};
-    },
-    render,
+const React = {
+    createElement,
     Component,
-
+    render,
 }
+
+export { render, createElement, Component }
+
+export default React
